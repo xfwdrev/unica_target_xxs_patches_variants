597ef659ac06e5230937a2b7810df1b65fc365ae
xfwdrev <frignacio16@gmail.com>
2026-02-23 00:25:40 +0800

    o1s: initial unified vendor
    Signed-off-by: xfwdrev <frignacio16@gmail.com>

---
+++ customize.sh
@@ -0,0 +1,24 @@
+for i in "odm" "vendor"; do
+    PROP="$i/etc/build.prop"
+    if [[ "$i" == "vendor" ]]; then
+        PROP="$i/build.prop"
+    fi
+
+    {
+        echo "# Added by target/o1s/patches/variants/customize.sh"
+        echo "import /$i/etc/sku/\${ro.boot.em.model}.prop"
+    } >> "$WORK_DIR/$PROP"
+
+    unset PROP
+done
+
+if ! grep -q "init_30_0 tee_file" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+    {
+        echo "(allow init_30_0 tee_file (dir (mounton)))"
+        echo "(allow priv_app_30_0 tee_file (dir (getattr)))"
+        echo "(allow init_30_0 vendor_fw_file (file (mounton)))"
+        echo "(allow priv_app_30_0 vendor_fw_file (file (getattr)))"
+        echo "(allow init_30_0 vendor_npu_firmware_file (file (mounton)))"
+        echo "(allow priv_app_30_0 vendor_npu_firmware_file (file (getattr)))"
+    } >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
+fi
